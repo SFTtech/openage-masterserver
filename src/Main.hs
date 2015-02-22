d9d5f2ff8d4e307660328258805ac0f0ae9d6964
Jonas Jelten <jj@sft.mx>
2015-02-23 00:57:44 +0100

    net: added basic argument parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,63 @@
 -- Copyright 2015-2015 the openage authors. See copying.md for legal info.
 
 import Control.Concurrent
-import Network.Socket
+import System.Exit
 import System.IO
 
+import qualified Data.Version as Ver
+import qualified Network.Socket as Sock
+import qualified Paths_openage_masterserver as Prg
+import qualified System.Environment as Env
+
 {-
- - sends a simple ohai to a tcp socket on port 4242
+ - main entry file for the openage masterserver
  -}
+
+
 
 main :: IO ()
 main = do
+  args <- Env.getArgs
+  if length args == 1 then
+    do
+      let mode = args !! 0
+      case mode of
+       "help" -> usage >> exit
+       "run" -> listenSock
+       _ -> usage >> die
+  else
+    usage >> die
+
+
+usage   = putStrLn ("openage master server " ++ (Ver.showVersion Prg.version)
+                    ++ "\n available options:\n" ++
+                    "  help: show this help\n" ++
+                    "  run: run the server")
+exit    = exitWith (ExitSuccess)
+die     = exitWith (ExitFailure 1)
+
+listenSock = do
   let port = 4242
   putStrLn "openage matchmaking and lobby server"
-  putStrLn $ "listening on port " ++ show port
-  sock <- socket AF_INET Stream 0
-  setSocketOption sock ReuseAddr 1
-  bindSocket sock (SockAddrInet port iNADDR_ANY)
-  listen sock 2
-  mainLoop sock
+  putStrLn ("listening on port " ++ show port)
+  sock <- Sock.socket Sock.AF_INET6 Sock.Stream 0
+  Sock.setSocketOption sock Sock.ReuseAddr 1
+  Sock.bind sock (Sock.SockAddrInet6 port 0 Sock.iN6ADDR_ANY 0)
+  Sock.listen sock 2
+  acceptLoop sock
 
 -- handle connections from the socket
-mainLoop :: Socket -> IO ()
-mainLoop sock = do
-  conn <- accept sock
+acceptLoop :: Sock.Socket -> IO ()
+acceptLoop sock = do
+  conn <- Sock.accept sock
   _ <- forkIO (runConn conn)
-  mainLoop sock
+  acceptLoop sock
 
 -- per-connection action
-runConn :: (Socket, SockAddr) -> IO ()
+runConn :: (Sock.Socket, Sock.SockAddr) -> IO ()
 runConn (sock, addr) = do
-  putStrLn $ "connection: " ++ show addr
-  hdl <- socketToHandle sock ReadWriteMode
+  putStrLn ("connection: " ++ show addr)
+  hdl <- Sock.socketToHandle sock ReadWriteMode
   hSetBuffering hdl NoBuffering
   hPutStrLn hdl "ohai"
   hClose hdl
