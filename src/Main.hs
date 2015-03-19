5faae7051ef78a9b61cd9b061387b856d43fc2c7
Jonas Jelten <jj@sft.mx>
2015-03-19 04:56:53 +0100

    main: implemented basic argparsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,17 @@
 -- Copyright 2015-2015 the openage authors. See copying.md for legal info.
 
+
+{- | main entry file for the openage masterserver
+ - this server will listen on a tcp socket
+ - and provide a funny API for gameservers and clients
+ - to start communicating with each other.
+ -}
 module Main where
 
 import System.Exit
+import Text.Printf
 
+import qualified Data.Map.Strict as Map
 import qualified Data.Version as Ver
 import qualified Paths_openage_masterserver as Prg
 import qualified System.Environment as Env
@@ -12,37 +20,64 @@ import qualified Config as Cfg
 import qualified Database as DB
 import qualified Server as Srv
 
+data CmdOption = NoOpt (IO ()) | CmdOption String (String -> IO ())
+data OptMap = Map String (String, CmdOption)
 
+opts = [("help",   ("show usage info", (NoOpt (usage >> exit)))),
+        ("run",    ("run the server",  (CmdOption "configfile" run))),
+        ("initdb", ("initialize the database", (CmdOption "configfile" initdb)))]
 
-{-
- - main entry file for the openage masterserver
- -}
-
+getElem idx xs =
+  if idx > (length xs) - 1 then
+    Nothing
+  else
+    Just (xs !! idx)
 
 main :: IO ()
 main = do
   args <- Env.getArgs
-  if length args == 2 then
-    do
-      let mode     = args !! 0
-      let cfgfile  = args !! 1
+  case (getElem 0 args) of
+   Just mode -> do
+     let o = Map.lookup mode (Map.fromList opts)
+     case o of
+      Just (_, opt) ->
+        runMode opt args
+      Nothing ->
+        usage >> die
+   Nothing -> usage >> die
 
-      cfg <- Cfg.readConfig cfgfile
+runMode :: CmdOption -> [String] -> IO ()
+runMode opt args =
+  case opt of
+   NoOpt f -> f
+   CmdOption _ f ->
+     case getElem 1 args of
+      Just name -> f name
+      Nothing -> error "missing argument"
 
-      case mode of
-       "help"    -> usage >> exit
-       "run"     -> Srv.listenSock
-       "initdb"  -> DB.initialize cfg
-       _         -> usage >> die
-  else
-    usage >> die
+run :: String -> IO ()
+run _ = do
+  Srv.listenSock
 
+initdb :: String -> IO ()
+initdb cfgfile = do
+  cfg <- Cfg.readConfig cfgfile
+  DB.initialize cfg
 
-usage   = putStrLn ("openage master server " ++ (Ver.showVersion Prg.version)
-                    ++ "\nusage: binary <mode> config.file"
-                    ++ "\navailable modes:\n"
-                    ++ "  help:    show this help\n"
-                    ++ "  initdb:  initialize the database schema\n"
-                    ++ "  run:     run the server")
+usage = putStrLn ("openage master server " ++ (Ver.showVersion Prg.version)
+                  ++ "\nusage: binary <mode> <mode options>\n"
+                  ++ "available modes:\n"
+                  ++ (modeString opts ""))
+
+modeString [] txt = txt
+modeString (x:xs) txt =
+  modeString xs (txt ++ printf "  %s%s: %s\n" (modename) (options) (modedesc))
+  where
+    modename = fst x
+    modedesc = (fst . snd) x
+    options = case (snd . snd) x of
+      NoOpt _ -> ""
+      CmdOption desc _ -> printf " <%s>" desc
+
 exit    = exitWith (ExitSuccess)
 die     = exitWith (ExitFailure 1)
