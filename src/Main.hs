8aed16443f53f2477066fff7551be9d6a1bee554
Jonas Jelten <jj@sft.mx>
2015-05-20 02:45:39 +0200

    main: merged functions requiring config file

--- src/Main.hs
+++ src/Main.hs
@@ -17,28 +17,44 @@ import qualified Config as Cfg
 import qualified Database as DB
 import qualified Server as Srv
 
+-- | available program run modes.
+data Action = Serve | InitDB
 
+-- | available command line options for running modes.
 opts :: [Args.OptEntry]
-opts = [("help",   ("show usage info",         (Args.NoOpt (putStrLn usage >> Args.exit)))),
-        ("run",    ("run the server",          (Args.CmdOption "configfile" run))),
-        ("initdb", ("initialize the database", (Args.CmdOption "configfile" initdb)))]
+opts = [("help",   ("show things that may help you", (Args.NoOpt (putStrLn help >> Args.exit)))),
+        ("run",    ("run the server",          (Args.CmdOption "configfile" (run Serve)))),
+        ("initdb", ("initialize the database", (Args.CmdOption "configfile" (run InitDB))))]
 
+-- | generate a string that displays the command line usage of this server.
 usage :: String
-usage = ("openage master server " ++ (Ver.showVersion Prg.version)
-         ++ "\nusage: binary <mode> <mode options>\n"
+usage = ("== openage master server " ++ (Ver.showVersion Prg.version)
+         ++ " ==\n\n"
+         ++ "usage: binary <mode> <mode options>\n\n"
          ++ (Args.modeString opts "available modes:\n"))
 
+-- | help about this program.
+help :: String
+help = ("-=[ openage master server ]=-\n"
+        ++ "  `<http://openage.sft.mx>´\n\n"
+        ++ "Copyright (C) 2015 the openage authors, GNU AGPLv3 or later.\n\n"
+        ++ "What does this program do?\n"
+        ++ " * registry for game lobbies\n"
+        ++ " * mediating between dedicated servers and game clients\n"
+        ++ "\n"
+        ++ "For more information and help,\n"
+        ++ "visit our IRC channel #sfttech on freenode.net")
+
+-- | entry point for this program.
 main :: IO ()
 main = do
   args <- Env.getArgs
   Args.parseArgs args opts usage
 
-run :: String -> IO ()
-run cfgfile = do
+-- | entry point of all actions the server can perform.
+run :: Action -> String -> IO ()
+run mode cfgfile = do
   cfg <- Cfg.readConfig cfgfile
-  Srv.listenSock cfg
-
-initdb :: String -> IO ()
-initdb cfgfile = do
-  cfg <- Cfg.readConfig cfgfile
-  DB.initialize cfg
+  case mode of
+    Serve -> Srv.listenSock cfg
+    InitDB -> DB.initialize cfg
