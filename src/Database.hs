7fd9f0a1d55123c4175d5e388c20f28e4faa33e8
Jonas Jelten <jj@sft.mx>
2015-05-20 02:46:56 +0200

    db: created and enabled first basic database schema

--- src/Database.hs
+++ src/Database.hs
@@ -8,8 +8,10 @@ import qualified Database.HDBC as DB
 import qualified Database.HDBC.PostgreSQL as PG
 
 import qualified Config as Cfg
+import qualified DBSchema as Schema
 
--- http://www.postgresql.org/docs/devel/static/libpq-connect.html#LIBPQ-CONNSTRING
+-- | Create a libpq connection string from a database config.
+-- <http://www.postgresql.org/docs/devel/static/libpq-connect.html#LIBPQ-CONNSTRING>
 connstring :: Cfg.Config -> String
 connstring cfg = printf "host=%s dbname=%s user=%s password=%s" h db u p
   where
@@ -18,18 +20,22 @@ connstring cfg = printf "host=%s dbname=%s user=%s password=%s" h db u p
     u  = Cfg.dbUser cfg
     p  = Cfg.dbPassword cfg
 
+-- | Connect to a postgresql database with a given config.
 connect :: Cfg.Config -> IO PG.Connection
 connect cfg = do
-  putStrLn "trying to connect to database..."
+  putStrLn "connecting to database..."
   con <- PG.connectPostgreSQL (connstring cfg)
   putStrLn "connected!"
   return con
 
-
+-- | Apply the schema to the database to initialize it
 initialize :: Cfg.Config -> IO ()
 initialize cfg = do
   con <- connect cfg
-  putStrLn "TODO: apply DB schema"
+  putStrLn "applying DB schema..."
+  DB.runRaw con Schema.schema
+  putStrLn "DB schema created."
+  DB.commit con
   DB.disconnect con
-  putStrLn "disconnected!"
+  putStrLn "disconnected from DB!"
   return ()
