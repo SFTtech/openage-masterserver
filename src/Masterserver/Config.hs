33874d6dcabd8c7189572602bb89bd6ad60928f3
tobhe <t.heider@campus.lmu.de>
2016-03-17 14:44:45 +0100

    Cleaned up Masterserver.Config Module

--- src/Masterserver/Config.hs
+++ src/Masterserver/Config.hs
@@ -8,36 +8,40 @@
 -- This Module defines the masterservers config parser.
 
 ------------------------------------------------------------------------------
-module Masterserver.Config (
-  getPostgresConf,
-  getPort,
-  getVersion
-  )where
+module Masterserver.Config
+       ( getPostgresConf
+       , getPort
+       , getVersion
+       ) where
 
-import Prelude hiding (concat, lookup, putStrLn)
 import Data.Aeson.TH
 import Data.Yaml
 import Database.Persist.Postgresql
 
-data Config = Config {
-  acceptedVersion :: [Int],
-  port :: Int,
-  database :: Value
+-- | Config datatype used for parsing.
+data Config = Config
+  { acceptedVersion :: [Int] -- ^ Client version accepted by server
+  , port :: Int              -- ^ Port to run server on
+  , database :: Value        -- ^ Database connection info
   } deriving Show
 
+-- | Derives aeson toJSON and fromJSON instances for Config.
 $(deriveJSON defaultOptions ''Config)
 
+-- | Get PostgresConf from yaml file.
 getPostgresConf :: IO PostgresConf
 getPostgresConf = do
   Just yaml <- decodeFile "etc/openage/masterserver.yaml"
   conf <- parseMonad loadConfig $ database yaml
   applyEnv (conf :: PostgresConf)
 
+-- | Get port from yaml file.
 getPort :: IO Int
 getPort = do
   Just yaml <- decodeFile "etc/openage/masterserver.yaml"
   return $ port yaml
 
+-- | Get accepted version from yaml file.
 getVersion :: IO [Int]
 getVersion = do
   Just yaml <- decodeFile "etc/openage/masterserver.yaml"
